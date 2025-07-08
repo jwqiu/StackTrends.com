43f5a4ec86a8dff60591e8a1d9d6cdc6e60c172d
Junwen-Qiu-1162541 <jwqiu1111@gmail.com>
2025-07-09 01:05:00 +1200

    Update config.js

--- 02_frontend/config.js
+++ 02_frontend/config.js
@@ -1,16 +1,16 @@
-// // config.js
+// config.js
 
-// // 本地调试时后端跑在 localhost:5001
-// const LOCAL_API = "https://localhost:5001";
+// 本地调试时后端跑在 localhost:5001
+const LOCAL_API = "https://localhost:5001";
 
-// // 正式环境后端和静态文件同域（或走相对路径）
-// const PROD_API = "https://stacktrends-api-cshjb2ephxbjdffa.newzealandnorth-01.azurewebsites.net";
+// 正式环境后端和静态文件同域（或走相对路径）
+const PROD_API = "https://stacktrends-api-cshjb2ephxbjdffa.newzealandnorth-01.azurewebsites.net";
 
-// // 判断当前是在本地开发（常见 localhost、127.0.0.1），还是线上
-// const host = window.location.hostname;
-// window.API_BASE =
-//   host === "localhost" || host === "127.0.0.1"
-//     ? LOCAL_API
-//     : PROD_API;
+// 判断当前是在本地开发（常见 localhost、127.0.0.1），还是线上
+const host = window.location.hostname;
+window.API_BASE =
+  host === "localhost" || host === "127.0.0.1"
+    ? LOCAL_API
+    : PROD_API;
 
-window.API_BASE = "";
+// window.API_BASE = "";
