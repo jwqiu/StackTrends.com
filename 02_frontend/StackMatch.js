366d7957f6032843343d05eabcbe4e20580b999b
Junwen-Qiu-1162541 <jwqiu1111@gmail.com>
2025-07-15 00:02:34 +1200

    Update StackMatch.js

--- 02_frontend/StackMatch.js
+++ 02_frontend/StackMatch.js
@@ -138,11 +138,7 @@ function renderJobs() {
           </div>
           <div class="mt-4 bg-gray-100 p-4 rounded shadow-md">
             <p class="inline-flex items-center text-sm text-gray-500 gap-1 mt-0">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" />
-                <path stroke-linecap="round" stroke-linejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" />
-                </svg>
-                Tech Requirements:
+                🖥️ Tech Requirements:
             </p>
             <p class="required-tech-stacks flex flex-wrap gap-1 text-sm mt-1">
               ${stacks}
