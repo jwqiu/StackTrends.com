2f2d918374c91e578fe84a67ca355744ddaf8b16
jwqiu <jwqiu1111@gmail.com>
2026-04-22 20:58:05 +1200

    Font size adjustment for job cards

--- 02_frontend/static/js/StackMatch.js
+++ 02_frontend/static/js/StackMatch.js
@@ -332,7 +332,7 @@ function renderJobs() {
               </p>
             </div>
             <div>
-              <p class="text-md text-gray-600 flex items-center gap-1">
+              <p class="text-sm text-gray-600 flex items-center gap-1">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="size-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
